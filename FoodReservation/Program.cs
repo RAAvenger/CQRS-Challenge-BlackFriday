150d07c8d8dd6b2c1de7453fa43abf0c4b7de401
Reza Adibi <adbrezaooo@gmail.com>
2024-07-12 16:23:33 +0330

    add otel support

--- FoodReservation/Program.cs
+++ FoodReservation/Program.cs
@@ -2,8 +2,22 @@ using FoodReservation.Application.Repositories.Abstractions;
 using FoodReservation.Infrastructure.BackgroundServices;
 using FoodReservation.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Logging.AddOpenTelemetry(options =>
+{
+    options.IncludeFormattedMessage = true;
+    options.IncludeScopes = true;
+    options.SetResourceBuilder(ResourceBuilder.CreateDefault()
+        .AddService(builder.Configuration["ServiceName"]!));
+    options.AddOtlpExporter();
+});
 
 // Add services to the container.
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
@@ -13,6 +27,19 @@ builder.Services.AddScoped<IFoodReservationsDbContext>(sp => sp.GetRequiredServi
 builder.Services.AddMediator(options => options.ServiceLifetime = ServiceLifetime.Transient);
 
 builder.Services.AddHostedService<DatabaseUpdater>();
+
+builder.Services
+    .AddOpenTelemetry()
+    .WithMetrics(options => options.AddAspNetCoreInstrumentation()
+        .AddHttpClientInstrumentation()
+        .AddRuntimeInstrumentation()
+        .AddProcessInstrumentation()
+        .AddPrometheusExporter())
+    .WithTracing(options => options.SetResourceBuilder(ResourceBuilder.CreateDefault()
+            .AddService(builder.Configuration["ServiceName"]!))
+        .AddAspNetCoreInstrumentation()
+        .AddHttpClientInstrumentation()
+        .AddOtlpExporter());
 
 builder.Services.AddDateOnlyTimeOnlyStringConverters();
 builder.Services.AddControllers();
@@ -35,6 +62,8 @@ app.UseCors(x => x
     .SetIsOriginAllowed(origin => true) // allow any origin
     //.WithOrigins("https://localhost:44351")); // Allow only this origin can also have multiple origins separated with comma
     .AllowCredentials()); // allow credentials
+
+app.UseOpenTelemetryPrometheusScrapingEndpoint();
 
 app.UseAuthorization();
 
