56489440a0ad07b201d0c9ac93138b7bb62348ff
Reza Adibi <adbrezaooo@gmail.com>
2024-07-31 19:01:24 +0330

    f

--- BlackFriday/Controllers/BlackFridayController.cs
+++ BlackFriday/Controllers/BlackFridayController.cs
@@ -1,31 +1,69 @@
-﻿using BlackFriday.Domain.Constants;
-using BlackFriday.Infrastructure.Controllers.Dtos;
+﻿using BlackFriday.Application.Repositories.Abstractions;
 using Mediator;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BlackFriday.Infrastructure.Controllers
 {
     [ApiController]
     public class BlackFridayController : ControllerBase
     {
+        private readonly IBlackFridaysDbContext _dbContext;
         private readonly IMediator _mediator;
 
-        public BlackFridayController(IMediator mediator)
+        public BlackFridayController(IMediator mediator, IBlackFridaysDbContext dbContext)
         {
             _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        }
+
+        [HttpGet("categories")]
+        public async Task<ActionResult<IReadOnlyCollection<string>>> GetAllCategories(CancellationToken cancellationToken)
+        {
+            var categories = await _dbContext.Products
+                .GroupBy(x => x.CategoryName)
+                .Select(x => x.Key)
+                .ToArrayAsync(cancellationToken);
+
+            return Ok(categories);
+        }
+
+        [HttpGet("items/{id}")]
+        public async Task<ActionResult<IReadOnlyCollection<Product>>> GetItemById([FromRoute] string id, CancellationToken cancellationToken)
+        {
+            var item = await _dbContext.Products
+                .FirstOrDefaultAsync(x => x.Asin == id, cancellationToken);
+            if (item is null)
+            {
+                return NotFound();
+            }
+            return Ok(item);
+        }
+
+        [HttpGet("categories/{category}")]
+        public async Task<ActionResult<IReadOnlyCollection<Product>>> GetItemsForGivenCategory([FromRoute] string category, CancellationToken cancellationToken)
+        {
+            var categoryItems = await _dbContext.Products
+                .Where(x => x.CategoryName == category)
+                .ToArrayAsync(cancellationToken);
+            if (categoryItems.Length == 0)
+            {
+                return NotFound();
+            }
+            return Ok(categoryItems);
         }
 
         //[HttpPost("deliver")]
         //public async Task<ActionResult> DeliverFoodToUser([FromBody] DeliverFoodToUserRequestDto request,
         //    CancellationToken cancellationToken)
         //{
-            //await _mediator.Send(new UpdateReservationStatusCommand
-            //{
-            //    FoodId = request.FoodId,
-            //    Date = request.Date,
-            //    Status = ReservationStatuses.Delivered,
-            //    UserId = request.UserId
-            //}, cancellationToken);
+        //await _mediator.Send(new UpdateReservationStatusCommand
+        //{
+        //    FoodId = request.FoodId,
+        //    Date = request.Date,
+        //    Status = ReservationStatuses.Delivered,
+        //    UserId = request.UserId
+        //}, cancellationToken);
         //    return Ok();
         //}
 
---
+++ BlackFriday/Persistence/DbMigrations/20240731152732_InitialCreate.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlackFriday.Infrastructure.Persistence.DbMigrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    asin = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    boughtInLastMonth = table.Column<long>(type: "bigint", nullable: false),
+                    categoryName = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    imgUrl = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    isBestSeller = table.Column<bool>(type: "boolean", nullable: false),
+                    price = table.Column<decimal>(type: "numeric(28,6)", precision: 28, scale: 6, nullable: false),
+                    productUrl = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    reviews = table.Column<long>(type: "bigint", nullable: false),
+                    stars = table.Column<decimal>(type: "numeric(28,6)", precision: 28, scale: 6, nullable: false),
+                    title = table.Column<string>(type: "character varying(1500)", maxLength: 1500, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.asin);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
