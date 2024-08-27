1e500b23c4c4844fde0d9f5fcc95c8f4b79f1a23
Reza Adibi <adbrezaooo@gmail.com>
2024-08-27 21:06:36 +0330

    f

--- BlackFriday.LoadTester/UseCases/Abstraction/ICategoriesItemsGetter.cs
+++
@@ -1,6 +0,0 @@
-namespace BlackFriday.LoadTester.UseCases.Abstraction;
-
-internal interface ICategoriesItemsGetter
-{
-	Task<IReadOnlyCollection<string>> GetCategoriesItemsAsync(IReadOnlyCollection<string> categories, CancellationToken cancellationToken);
-}
