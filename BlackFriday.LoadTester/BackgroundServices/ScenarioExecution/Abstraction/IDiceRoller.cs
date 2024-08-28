cc70ff5be0cc6f8f4d45c7e5eca750fdf90a3d36
Reza Adibi <adbrezaooo@gmail.com>
2024-08-28 20:23:36 +0330

    remove load tester release 2 (#5)

--- BlackFriday.LoadTester/BackgroundServices/ScenarioExecution/Abstraction/IDiceRoller.cs
+++
@@ -1,8 +0,0 @@
-﻿using BlackFriday.LoadTester.BackgroundServices.ScenarioExecution.Enums;
-
-namespace BlackFriday.LoadTester.BackgroundServices.ScenarioExecution.Abstraction;
-
-internal interface IDiceRoller
-{
-	LoadTestScenarioType RollTheDice();
-}
