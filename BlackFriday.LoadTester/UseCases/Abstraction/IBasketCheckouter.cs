2d755268f960b2c8c4791b09dff4f7354e99c71d
Reza Adibi <adbrezaooo@gmail.com>
2024-08-28 20:13:52 +0330

    remove load tester (#3)

--- BlackFriday.LoadTester/UseCases/Abstraction/IBasketCheckouter.cs
+++
@@ -1,6 +0,0 @@
-namespace BlackFriday.LoadTester.UseCases.Abstraction;
-
-internal interface IBasketCheckouter
-{
-	Task CheckoutBasketAsync(string userId, string basketId, CancellationToken cancellationToken);
-}
--- BlackFriday.LoadTester/UseCases/Abstraction/IItemsGetter.cs
+++
@@ -1,6 +0,0 @@
-namespace BlackFriday.LoadTester.UseCases.Abstraction;
-
-internal interface IItemsGetter
-{
-	Task GetItemsAsync(IReadOnlyCollection<string> itemIds, CancellationToken cancellationToken);
-}
