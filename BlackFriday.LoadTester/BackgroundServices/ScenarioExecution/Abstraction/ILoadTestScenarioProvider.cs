63b8bd6a4c03c07ad2fdc5c0ba966cc8e128a48b
Reza Adibi <adbrezaooo@gmail.com>
2024-08-28 20:13:52 +0330

    remove load tester (#3)

--- BlackFriday.LoadTester/BackgroundServices/ScenarioExecution/Abstraction/ILoadTestScenarioProvider.cs
+++
@@ -1,8 +0,0 @@
-﻿using BlackFriday.LoadTester.BackgroundServices.ScenarioExecution.Enums;
-
-namespace BlackFriday.LoadTester.BackgroundServices.ScenarioExecution.Abstraction;
-
-internal interface ILoadTestScenarioProvider
-{
-	ILoadTestScenario MakeScenario(LoadTestScenarioType scenarioType);
-}
--- BlackFriday.LoadTester/UseCases/Abstraction/IBasketFiller.cs
+++
@@ -1,6 +0,0 @@
-namespace BlackFriday.LoadTester.UseCases.Abstraction;
-
-internal interface IBasketFiller
-{
-	Task FillBasketAsync(string userId, string basketId, IReadOnlyCollection<string> itemIds, CancellationToken cancellationToken);
-}
