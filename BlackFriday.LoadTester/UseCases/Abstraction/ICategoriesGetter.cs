c316e4f4c7e4b333d845c824b083d939efc68d22
Reza Adibi <adbrezaooo@gmail.com>
2024-08-28 20:13:52 +0330

    remove load tester (#3)

--- BlackFriday.LoadTester/UseCases/Abstraction/ICategoriesGetter.cs
+++
@@ -1,6 +0,0 @@
-namespace BlackFriday.LoadTester.UseCases.Abstraction;
-
-internal interface ICategoriesGetter
-{
-	Task<IReadOnlyCollection<string>> GetCategoriesAsync(CancellationToken cancellationToken);
-}
